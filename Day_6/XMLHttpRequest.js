f9928d1fe07f487b12fabb77b0e65f8ec36530ba
Vignesh R <rajuvignesh91@gmail.com>
2024-07-05 23:14:45 +0530

    day 6 task1

--- Day_6/XMLHttpRequest.js
+++ Day_6/XMLHttpRequest.js
@@ -1,34 +1,26 @@
-var todoreq = new XMLHttpRequest()
+var req = new XMLHttpRequest()
 var web = "https://restcountries.com/v3.1/all"
-todoreq.open("GET",web)
-todoreq.send()
-todoreq.onreadystatechange = function(){
-    const data = JSON.parse (this.response)
-    
-    function Continent(Element){
-        return Element.continents == "Asia"
-    }
-    function populate(Element){
-        return(Element.population < 200000  )  
-
-    }
-    function totpop(carry,next){
-       // console.log(Number(carry.population), "+" , Number(next.population))
-        return(carry + Number.parseInt(next.population)   )
-    }
-    function curr(Element){
-        console.log(Element.currencies )
-        return (Element.currencies == "INR")
-    }
-    
-
-    const data1 = data.filter(Continent)
-    console.log(data1)
-    const data2 = data.filter(populate)
-    console.log(data2)
-    const data3 = data.reduce(totpop)
-    console.log(data3)
-    const data4 = data.filter(curr)
-    console.log(data4)
-    data.forEach((Element) =>{console.log(Element.capital,Element.name,Element.flag)})
+req.open("GET",web)
+req.send()
+req.onreadystatechange = function () {
+    const arr = JSON.parse(this.response)
+    const T1 =arr.filter((item) => {
+       return (item.continents == "Asia")
+    })
+    const T2 = arr.filter((item) => {
+        return ( Number(item.population) < 200000 )
+    })
+    arr.forEach((item) =>{
+        console.log(item.name,item.capital,item.flag)
+    })
+    const T3 = arr.reduce((total,next) => {
+         return(total + next.population)
+    },0)
+    const T4 = arr.filter((item) => {
+        return (item.currencies = "USD")
+    })
+    console.log(T1)
+    console.log(T2)
+    console.log(T3)
+    console.log(T4)
 }
